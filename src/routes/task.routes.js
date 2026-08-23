56e73d14c5e2d7782d2d023bfc27262451d0ea6f
diegoriveros17 <diegoriveros1991@gmail.com>
2026-08-22 23:00:04 -0300

    feat: se finalizo las validaciones de la entidad task

--- src/routes/task.routes.js
+++ src/routes/task.routes.js
@@ -6,15 +6,23 @@ import {
   updateTask,
   deleteTask,
 } from "../controllers/task.controller.js";
+// import { body } from "express-validator";
+import { validate } from "../middlewares/validate.js";
+import {
+  createTaskValidation,
+  deleteTaskValidation,
+  getTaskValidation,
+  updateTaskValidation,
+} from "../middlewares/validations/task.validation.js";
 
 export const taskRouter = Router();
 
 taskRouter.get("/tasks", getAllTasks);
 
-taskRouter.get("/tasks/:id", getTaskById);
+taskRouter.get("/tasks/:id", getTaskValidation, validate, getTaskById);
 
-taskRouter.post("/tasks", insertTask);
+taskRouter.post("/tasks", createTaskValidation, validate, insertTask);
 
-taskRouter.put("/tasks/:id", updateTask);
+taskRouter.put("/tasks/:id", updateTaskValidation, validate, updateTask);
 
-taskRouter.delete("/tasks/:id", deleteTask);
+taskRouter.delete("/tasks/:id", deleteTaskValidation, validate, deleteTask);
